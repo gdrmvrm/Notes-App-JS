cc267c4c141bd3632d653b40c0f74047ebb6ea91
GDRMVRM <gdrmvrm@gmail.com>
2022-04-07 07:43:50 +0100

    implemented 'addNewNote()' so that the user can add notes on the page

--- notesView.js
+++ notesView.js
@@ -2,6 +2,13 @@ class NotesView {
   constructor(model) {
     this.model = model;
     this.mainContainer = document.querySelector("#main-container");
+    this.buttonEl = document.querySelector("#add-notes-button");
+    this.inputEl = document.querySelector("#note-input");
+
+    this.buttonEl.addEventListener("click", () => {
+      const newNote = this.inputEl.value;
+      this.addNewNotes(newNote);
+    });
   }
 
   displayNotes() {
@@ -13,6 +20,11 @@ class NotesView {
       noteEl.className = "note";
       this.mainContainer.append(noteEl);
     });
+  }
+
+  addNewNotes(newNote) {
+    this.model.addNote(newNote);
+    this.displayNotes();
   }
 }
 
--- notesView.test.js
+++ notesView.test.js
@@ -5,7 +5,7 @@ const NotesView = require("./notesView");
 const NotesModel = require("./notesModel");
 const fs = require("fs");
 
-describe("Page View", () => {
+describe("NotesView", () => {
   it("displays 2 notes", () => {
     document.body.innerHTML = fs.readFileSync("./index.html");
 
@@ -16,5 +16,22 @@ describe("Page View", () => {
     view.displayNotes();
 
     expect(document.querySelectorAll("div.note").length).toBe(2);
+  });
+
+  it("adds a new note", () => {
+    document.body.innerHTML = fs.readFileSync("./index.html");
+
+    const model = new NotesModel();
+    const view = new NotesView(model);
+    const buttonEl = document.querySelector("#add-notes-button");
+    const inputEl = document.querySelector("#note-input");
+    inputEl.value = "Hello there!";
+    buttonEl.click();
+
+    buttonEl.class = "note";
+    expect(document.querySelectorAll("div.note").length).toEqual(1);
+    expect(document.querySelectorAll("div.note")[0].innerText).toEqual(
+      "Hello there!"
+    );
   });
 });
